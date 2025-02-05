4923995f989843019b55b19e06b461e2d2e94eac
daniel.ruiz-gil@iesruizgijon.com <tuemail@example.com>
2025-02-05 12:59:20 +0100

    Añadido constructor

--- src/main/java/com/example/proyecto/Tarea.java
+++ src/main/java/com/example/proyecto/Tarea.java
@@ -17,4 +17,9 @@ public class Tarea {
 
     private String titulo;
     private String descripcion;
+
+    public Tarea(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
 }
